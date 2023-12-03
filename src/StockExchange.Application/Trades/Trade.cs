08320df9d3babe029ee56a720babe94f7a9149c2
drgonzobrighton <mihaly_kisvarday@yahoo.com>
2023-12-03 09:38:50 +0000

    init

---
+++ src/StockExchange.Application/Trades/Trade.cs
@@ -0,0 +1,20 @@
+﻿namespace StockExchange.Application.Trades;
+
+public class Trade
+{
+    public Guid Id { get; internal init; }
+    public string TickerSymbol { get; internal init; }
+    public TradeType Type { get; internal init; }
+    public decimal Price { get; internal init; }
+    public decimal NumberOfShares { get; internal init; }
+    public Guid BrokerId { get; internal init; }
+    public DateTime CreatedAt { get; internal init; }
+}
+
+public enum TradeType
+{
+    Unknown,
+    Sell,
+    Buy
+}
+
