8c2a640594393a9a7d31d5764f5c5c10f6820c6e
drgonzobrighton <mihaly_kisvarday@yahoo.com>
2023-12-03 16:46:59 +0000

    add getRange

--- src/StockExchange.Application/Stocks/GetRange.cs
+++ src/StockExchange.Application/Stocks/GetRange.cs
@@ -1,10 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using StockExchange.Application.Common;
+using StockExchange.Application.StockSnapshots;
 
 namespace StockExchange.Application.Stocks;
-internal class GetRange
+
+public record GetStockRangeQuery(string Symbols);
+
+public sealed class GetStockRangeQueryHandler(IStockSnapshotRepository repository)
 {
+    public async Task<Result<List<Stock>>> Handle(GetStockRangeQuery query, CancellationToken cancellationToken = default)
+    {
+        if (string.IsNullOrWhiteSpace(query.Symbols))
+            return new ResultErrors("At least one ticker symbol is required");
+
+        var symbols = query.Symbols.Split(',').Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
+
+        if (!symbols.Any())
+            return new ResultErrors("At least one ticker symbol is required");
+
+        var snapshots = await repository.GetRange(symbols, cancellationToken);
+
+
+        return snapshots.Select(Stock.FromSnapshot).ToList();
+    }
 }
--- src/StockExchange.Application/Stocks/Stock.cs
+++ src/StockExchange.Application/Stocks/Stock.cs
@@ -4,19 +4,21 @@ namespace StockExchange.Application.Stocks;
 
 public class Stock
 {
-    internal Stock(string tickerSymbol, decimal value)
+    internal Stock(string tickerSymbol, decimal value, DateTime timestamp)
     {
         Value = value;
         TickerSymbol = tickerSymbol;
+        Timestamp = timestamp;
     }
 
     public string TickerSymbol { get; }
     public decimal Value { get; }
+    public DateTime Timestamp { get; }
 
     public static Stock FromSnapshot(StockSnapshot snapshot)
     {
         var stockPrice = snapshot.TotalShares != 0 ? snapshot.TotalValue / snapshot.TotalShares : 0;
 
-        return new(snapshot.TickerSymbol, stockPrice);
+        return new(snapshot.TickerSymbol, stockPrice, snapshot.LatestTradeDate);
     }
 }
